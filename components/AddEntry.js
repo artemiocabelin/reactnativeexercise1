3e4ec0ee368e28921a3eee8014bfcaface7dc5f9
Artemio Cabelin <artemiocabelin@gmail.com>
2017-11-17 19:10:30 -0800

    add geolocation, notifications, animations

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import  { View, Text, TouchableOpacity, Platform, StyleSheet } from 'react-native'
-import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
+import { getMetricMetaInfo, timeToString, getDailyReminderValue, clearLocalNotification, setLocalNotification } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider' 
 import UdaciSteppers from './UdaciSteppers' 
 import DateHeader from './DateHeader' 
@@ -80,7 +80,8 @@ class AddEntry extends Component {
 
     submitEntry({ entry, key })
 
-    // Clear local notification
+    clearLocalNotification()
+      .then(setLocalNotification)
   }
 
   reset = () => {
